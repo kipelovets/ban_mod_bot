67994168969a51798a73da4f3e54983b55f63c8c
Georgii Korshunov <kipelovets@mail.ru>
2022-05-10 11:53:13 +0200

    WIP

--- bot/__init__.py
+++
@@ -1,5 +0,0 @@
-from .my import myfunc
-
-__all__ = (
-  'myfunc'
-)
--- bot/handlers.py
+++ bot/handlers.py
@@ -6,15 +6,39 @@ from telegram.ext import (Updater, CallbackContext, CommandHandler)
 
 def echo(update: Update, context: CallbackContext):
     context.bot.send_message(
-        chat_id=update.effective_chat.id, text=update.message.text)
+        chat_id=update.effective_chat.id, text="Click here: <a href=\"tel://112\">112</a>", parse_mode="HTML")
 
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(
         chat_id=update.effective_chat.id, text="Hello, I'm a bot")
 
+def delete_join_messages():
+    pass
+
+def delete_leave_messages():
+    pass
+
+def delete_bots():
+    pass
+
+def ban_bot_inviters():
+    pass
+
+def ban_arabic_usernames():
+    pass
+
+def delete_arabic_messages():
+    pass
+
+def ban_if_first_message_has_links():
+    pass
+
+def welcome_message():
+    pass
 
 def chat_member(update: Update, context: CallbackContext):
+    
     user = update.chat_member.new_chat_member.user
     logging.debug(f"chat member id={user.id} fname={user.first_name} lname={user.last_name} uname={user.username} is_bot={user.is_bot}")
 
