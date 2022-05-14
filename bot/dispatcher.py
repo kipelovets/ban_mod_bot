74df90b9a87ad640124a67188f6ec170586fc691
Georgii Korshunov <kipelovets@mail.ru>
2022-05-14 21:29:33 +0200

    Back to beginning button

--- bot/dispatcher.py
+++ bot/dispatcher.py
@@ -3,12 +3,14 @@ from aiogram import Dispatcher
 
 from bot.storage import TranslatorsData
 from .handlers import (make_choose_translator,
-                       make_select_language, start, echo, cb)
+                       make_select_language, select_from_language, start, echo, cb)
 
 
 def configure_dispatcher(dispatcher: Dispatcher, data: TranslatorsData):
     dispatcher.register_message_handler(start, commands="start")
     dispatcher.register_message_handler(echo)
+    dispatcher.register_callback_query_handler(
+        select_from_language, cb.filter(from_lang=[""], to_lang=[""]))
     dispatcher.register_callback_query_handler(
         make_select_language(data), cb.filter(to_lang=[""]))
     dispatcher.register_callback_query_handler(
--- bot/handlers.py
+++ bot/handlers.py
@@ -11,16 +11,26 @@ cb = CallbackData("l", "user_id", "from_lang",
 from_languages = ["украинский", "русский"]
 
 
-def make_cb(user_id: int, from_lang: str, to_lang: str = "", prev_translator: str = ""):
+def make_cb(user_id: int, from_lang: str = "", to_lang: str = "", prev_translator: str = ""):
     to_lang = code_by_lang(to_lang) if to_lang != "" else ""
-    return cb.new(user_id=user_id, from_lang=code_by_lang(from_lang), to_lang=to_lang, prev_translator=prev_translator)
+    from_lang = code_by_lang(from_lang) if from_lang != "" else ""
+    return cb.new(user_id=user_id, from_lang=from_lang, to_lang=to_lang, prev_translator=prev_translator)
 
 
 def format_name(user: types.User) -> str:
-    if user.username != "" and user.username != None:
-        return f"@{user.username}"
-    else:
-        return f"{user.full_name}"
+    return user.username if user.username != "" and user.username != None else user.full_name
+
+
+def format_from_language_keyboard(user_id: int) -> types.InlineKeyboardMarkup:
+    keyboard = types.InlineKeyboardMarkup()
+    for lang in from_languages:
+        keyboard.add(types.InlineKeyboardButton(
+            text=lang, callback_data=make_cb(user_id, lang)))
+    return keyboard
+
+
+def format_from_language_message(username: str) -> str:
+    return f"Привет @{username}!\nВыберите язык с которого нужно перевести"
 
 
 async def echo(message: types.Message):
@@ -28,11 +38,15 @@ async def echo(message: types.Message):
 
 
 async def start(message: types.Message):
-    keyboard = types.InlineKeyboardMarkup()
-    for lang in from_languages:
-        keyboard.add(types.InlineKeyboardButton(
-            text=lang, callback_data=make_cb(message.from_user.id, lang)))
-    await message.answer(f"Привет @{format_name(message.from_user)}!\nВыберите язык с которого нужно перевести", reply_markup=keyboard)
+    await message.answer(format_from_language_message(format_name(message.from_user)), reply_markup=format_from_language_keyboard(message.from_user.id))
+
+
+async def select_from_language(call: types.CallbackQuery, callback_data: dict):
+    user_id = int(callback_data["user_id"])
+    if user_id != call.from_user.id:
+        await call.answer("Вы не можете отвечать на чужое сообщение!")
+        return
+    await call.message.edit_text(format_from_language_message(format_name(call.from_user)), reply_markup=format_from_language_keyboard(call.from_user.id))
 
 
 def make_select_language(data: TranslatorsData):
@@ -57,6 +71,9 @@ def make_select_language(data: TranslatorsData):
             else:
                 keyboard.add(types.InlineKeyboardButton(text=lang, callback_data=make_cb(
                     call.from_user.id, from_lang, lang)))
+
+        keyboard.add(types.InlineKeyboardButton(
+            text="Назад", callback_data=make_cb(call.from_user.id)))
 
         await call.message.edit_text(f"Привет @{format_name(call.from_user)}!\nВыбранный язык документа: {from_lang}\nТеперь выберите язык на который нужно перевести: ", reply_markup=keyboard)
 
