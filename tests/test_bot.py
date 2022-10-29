659029c48e40e830849c245c45682f9aae6f39ee
Georgii Korshunov <kipelovets@mail.ru>
2022-10-29 18:30:11 +0200

    Refactored assertions

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -30,20 +30,30 @@ async def test_handler_welcome():
     chat_member_mock = Mock(
         new_chat_member=new_chat_member_mock, bot=bot_mock, chat=chat_mock)
     await welcome(chat_member=chat_member_mock)
-    bot_mock.send_message.assert_called_once()
-    assert 2 == len(bot_mock.send_message.call_args.args)
-    assert chat_member_mock.chat.id == bot_mock.send_message.call_args.args[0]
-    expected_message = 'Привет @Joss!\nВыберите язык с которого нужно перевести'
-    assert expected_message == bot_mock.send_message.call_args.args[1]
-    assert 1 == len(bot_mock.send_message.call_args.kwargs)
-    markup = bot_mock.send_message.call_args.kwargs["reply_markup"]
-    assert isinstance(markup, types.InlineKeyboardMarkup)
-    assert 2 == len(markup.inline_keyboard)
-    assert 1 == len(markup.inline_keyboard[0])
-    button = markup.inline_keyboard[0][0]
-    assert "украинский" == button.text
-    assert 'l|123|ua||' == button.callback_data
+    assert_message_sent(bot_mock, chat_member_mock.chat.id,
+                        'Привет @Joss!\nВыберите язык с которого нужно перевести',
+                        [
+                            ["украинский", 'l|123|ua||'],
+                            ["русский", 'l|123|ru||'],
+                        ])
 
 
 def create_mock_user() -> types.User:
     return Mock(id=ID, username=NAME, full_name=NAME)
+
+
+def assert_message_sent(bot_mock: AsyncMock, chat_id: AsyncMock,
+                        expected_message: str, buttons: list[list[str]]):
+    bot_mock.send_message.assert_called_once()
+    assert 2 == len(bot_mock.send_message.call_args.args)
+    assert chat_id == bot_mock.send_message.call_args.args[0]
+    assert expected_message == bot_mock.send_message.call_args.args[1]
+    assert 1 == len(bot_mock.send_message.call_args.kwargs)
+    markup = bot_mock.send_message.call_args.kwargs["reply_markup"]
+    assert isinstance(markup, types.InlineKeyboardMarkup)
+    assert len(buttons) == len(markup.inline_keyboard)
+    for button_index, expected_button in enumerate(buttons):
+        assert 1 == len(markup.inline_keyboard[button_index])
+        button = markup.inline_keyboard[button_index][0]
+        assert expected_button[0] == button.text
+        assert expected_button[1] == button.callback_data
